1fc56f759c5cc800d2244afca16f810b28e5c028
Delmar-Ty <reliablesource13@gmail.com>
2022-03-23 18:25:57 -0700

    Made it work

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -3,16 +3,21 @@ let cards = document.querySelectorAll('.card');
 
 let list = [];
 
-fetch(url).then(res => res.json().then(data => {
-    list = data;
-    cards.forEach((el, index) => {
-        addInfo(index);
-        el.addEventListener('click', function() {
-            console.log(this);
-            console.log(this.id);
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        list = data;
+        cards.forEach((el, index) => {
+            addInfo(index);
+            el.addEventListener('click', function() {
+                document.querySelector('.map').innerHTML = `<iframe id="map-box" src="${list[this.id].map}" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`;
+                let infoList = document.querySelectorAll('.list-item');
+                infoList[0].textContent = list[this.id].name;
+                infoList[1].textContent = list[this.id].address;
+            })
         })
-    });
-}));
+    })
+    .catch(err => console.error);
 
 function addInfo(index) {
     let title = document.querySelectorAll('.card-title');
